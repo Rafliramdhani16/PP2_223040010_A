dc3ef728bd651e60bdcfaa610cdf73ed82c760d8
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-09-23 21:53:46 +0700

    Pertemuan 1

--- Pertemuan1/src/HelloWorldSwing.java
+++ Pertemuan1/src/HelloWorldSwing.java
@@ -2,23 +2,17 @@ import javax.swing.*;
 
 public class HelloWorldSwing {
     private static void createAndShowGUI() {
-        // Membuat JFrame dengan judul "HelloWorldSwing"
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Menambahkan JLabel dengan teks "Hello World"
-        JLabel label = new JLabel("Hello World");
+        JLabel label = new JLabel("Hello, Rafli");
         frame.getContentPane().add(label);
 
-        // Menyesuaikan ukuran frame sesuai dengan isinya
         frame.pack();
-
-        // Menampilkan frame
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Memastikan GUI dijalankan di thread event-dispatching
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
